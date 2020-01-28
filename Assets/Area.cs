60d531a45fe5f6cbee96f725b485f0e8cfc138f7
Alex <alexey-bycov@yandex.ru>
2020-01-28 16:13:04 +0700

    Add Area class

--- Assets/Area.cs
+++ Assets/Area.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Area : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isWalkable = true;
+    [SerializeField] float weight = 1;
+    [SerializeField] bool isTramplable = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool IsWalkable => isWalkable;
+    public float Weight => weight;
+    public bool IsTramplable => isTramplable;
 }
