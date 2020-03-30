0c9ac9b5d8a3f83b788bf0f825065f46d21f2f21
Alex <alexey-bycov@yandex.ru>
2020-03-30 22:33:27 +0700

    Add litte button in top left corner of scene view invoking GraphBuilder.Build()

---
+++ Assets/BuildGraphTool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildGraphTool : Editor
+{
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += OnScene;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnScene;
+    }
+
+    private static void OnScene(SceneView sceneview)
+    {
+        Handles.BeginGUI();
+
+        GUI.color = new Color(1, 1, 1, 0.4f);
+        if (GUI.Button(new Rect(2, 2, 40, 15), "Build"))
+            FindObjectOfType<GraphBuilder>().Build();
+
+        Handles.EndGUI();
+    }
+}
