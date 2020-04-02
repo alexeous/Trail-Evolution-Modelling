ec771bbafc257b281ef5526e15f8a9c1532f9a77
Alex <alexey-bycov@yandex.ru>
2020-04-02 17:13:12 +0700

    Remove commented path finding code

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -43,27 +43,13 @@ public class PathFinder
 
         Cleanup(open, graph);
         return null;
+    }
 
 
-        //float bound = HeuristicCost(start.Position, goal.Position);
-        //var path = new Stack<Node>();
-        //path.Push(start);
-
-        //while (true)
-        //{
-        //    float t = Search(path, goal, 0, bound);
-
-        //    if (t == Found)
-        //    {
-        //        return path.ToArray();
-        //    }
-        //    if (t == float.PositiveInfinity)
-        //    {
-        //        return null;
-        //    }
-
-        //    bound = t;
-        //}
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static float HeuristicCost(Node node, Node goal)
+    {
+        return Vector2.Distance(node.Position, goal.Position);
     }
 
     private static void Cleanup(FastPriorityQueue<Node> open, Graph graph)
@@ -89,53 +75,4 @@ public class PathFinder
         pathReversed.Reverse();
         return pathReversed.ToArray();
     }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static float HeuristicCost(Node node, Node goal)
-    {
-        return Vector2.Distance(node.Position, goal.Position);
-
-        //float dx = Mathf.Abs(nodePos.x - goalPos.x);
-        //float dy = Mathf.Abs(nodePos.y - goalPos.y);
-        //return dx + dy + (Sqrt2 - 2) * Mathf.Min(dx, dy);
-    }
-
-
-
-    //private static float Search(Stack<Node> path, Node goal, float currentCost, float bound)
-    //{
-    //    Node node = path.Peek();
-    //    float f = currentCost + HeuristicCost(node.Position, goal.Position);
-
-    //    if (f > bound)
-    //    {
-    //        return f;
-    //    }
-    //    if (node == goal)
-    //    {
-    //        return Found;
-    //    }
-
-    //    float min = float.PositiveInfinity;
-    //    List<Edge> incidentEdges = node.IncidentEdges;
-    //    for (int i = 0; i < incidentEdges.Count; i++)
-    //    {
-    //        Edge edge = incidentEdges[i];
-    //        Node successor = edge.GetOppositeNode(node);
-
-    //        if (path.Contains(successor)) continue;
-
-    //        path.Push(successor);
-
-    //        float t = Search(path, goal, currentCost + edge.Weight, bound);
-    //        if (t == Found)
-    //        {
-    //            return Found;
-    //        }
-    //        min = Mathf.Min(min, t);
-
-    //        path.Pop();
-    //    }
-    //    return min;
-    //}
 }
