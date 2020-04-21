d2b7ccb0ff1370b54ff64f01afd0d3a606ace526
Alex <alexey-bycov@yandex.ru>
2020-04-21 22:13:07 +0700

    Add GraphTypes.Attractor class

---
+++ GraphTypes/Attractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailEvolutionModelling.GraphTypes
+{
+    [Serializable]
+    public class Attractor
+    {
+        public Node Node { get; set; }
+        public bool IsSource { get; set; }
+        public bool IsDrain { get; set; }
+        public float Performance { get; set; }
+        public float WorkingRadius { get; set; }
+    }
+}
