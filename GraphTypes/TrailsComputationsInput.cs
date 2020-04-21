8a6f8b768ea2d3fc6fd0c86fc315b48a36d91add
Alex <alexey-bycov@yandex.ru>
2020-04-21 22:13:49 +0700

    Add TrailsComputationsInput and TrailsComputationsOutput classes used for data transfer between the program and the GPU proxy

---
+++ GraphTypes/TrailsComputationsInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailEvolutionModelling.GraphTypes
+{
+    [Serializable]
+    public class TrailsComputationsInput
+    {
+        public Graph Graph { get; set; }
+        public Attractor[] Attractors { get; set; }
+    }
+}
