d841b949b068a13ca5f87b3ce5369de8d7c2b84b
Alex <alexey-bycov@yandex.ru>
2020-04-26 22:41:09 +0700

    Decorate AreaAttributes with [Serializable] instead of implementing IXmlSerializable

--- TrailEvolutionModelling/MapObjects/AreaAttributes.cs
+++ TrailEvolutionModelling/MapObjects/AreaAttributes.cs
@@ -9,22 +9,11 @@ using System.Xml.Serialization;
 
 namespace TrailEvolutionModelling.MapObjects
 {
-    struct AreaAttributes : IXmlSerializable
+    [Serializable]
+    struct AreaAttributes
     {
         public bool IsWalkable { get; set; }
         public float Weight { get; set; }
         public bool IsTramplable { get; set; }
-
-        public XmlSchema GetSchema() => null;
-
-        public void ReadXml(XmlReader reader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteXml(XmlWriter writer)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
