c92e4c8419cb5f6084321ef473d2708709e05df1
Alex <alexey-bycov@yandex.ru>
2020-04-26 16:53:12 +0700

    Add AreaAttributes struct

---
+++ TrailEvolutionModelling/MapObjects/AreaAttributes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace TrailEvolutionModelling.MapObjects
+{
+    struct AreaAttributes : IXmlSerializable
+    {
+        public bool IsWalkable { get; set; }
+        public float Weight { get; set; }
+        public bool IsTramplable { get; set; }
+
+        public XmlSchema GetSchema() => null;
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
