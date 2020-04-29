8401d33edeb1766a4c8151ba3afa6d1f4196fccd
Alex <alexey-bycov@yandex.ru>
2020-04-29 22:58:35 +0700

    Create MapObjectTool to be the parent of PolygonTool

---
+++ TrailEvolutionModelling/EditorTools/MapObjectTool.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.UI.Wpf;
+using TrailEvolutionModelling.MapObjects;
+using TrailEvolutionModelling.Util;
+
+namespace TrailEvolutionModelling.EditorTools
+{
+    abstract class MapObjectTool<T> where T : MapObject
+    {
+        protected MapControl MapControl { get; }
+        protected WritableLayer TargetLayer { get; }
+        protected AreaType CurrentAreaType { get; set; }
+        protected T CurrentDrawnObject { get; set; }
+
+        public bool IsInDrawingMode { get; private set; }
+
+        private System.Windows.Point mouseDownPos;
+        private Mapsui.Geometries.Point previewPoint;
+        private Feature previewPointFeature;
+
+
+        public MapObjectTool(MapControl mapControl, WritableLayer polygonLayer)
+        {
+            this.MapControl = mapControl;
+            this.TargetLayer = polygonLayer;
+
+            this.MapControl.MouseLeftButtonDown += OnLeftMouseDown;
+            this.MapControl.MouseLeftButtonUp += OnLeftMouseUp;
+            this.MapControl.MouseMove += OnMouseMove;
+        }
+
+        public void BeginDrawing(AreaType areaType)
+        {
+            if (IsInDrawingMode)
+            {
+                return;
+            }
+
+            IsInDrawingMode = true;
+            CurrentAreaType = areaType;
+            MapControl.Cursor = Cursors.Pen;
+        }
+
+
+        private void OnLeftMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsInDrawingMode)
+            {
+                return;
+            }
+            mouseDownPos = e.GetPosition(MapControl);
+        }
+
+        private void OnLeftMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsInDrawingMode)
+            {
+                return;
+            }
+            const double tolerance = 5;
+            var pos = e.GetPosition(MapControl);
+            if (Math.Abs(pos.X - mouseDownPos.X) > tolerance ||
+                Math.Abs(pos.Y - mouseDownPos.Y) > tolerance)
+            {
+                return;
+            }
+            // previewPoint is already in currentPolygon.
+            // Setting it null makes OnMouseMove not remove it from currentPolygon
+            // thus it stays persistently
+            previewPoint = null;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsInDrawingMode)
+            {
+                return;
+            }
+
+            if (CurrentDrawnObject == null)
+            {
+                CurrentDrawnObject = CreateNewMapObject();
+                CurrentDrawnObject.AreaType = CurrentAreaType;
+                TargetLayer.Add(CurrentDrawnObject);
+            }
+            if (previewPoint != null)
+            {
+                CurrentDrawnObject.Vertices.Remove(previewPoint);
+            }
+            previewPoint = GetGlobalPointFromEvent(e);
+            CurrentDrawnObject.Vertices.Add(previewPoint);
+
+            if (previewPointFeature == null)
+            {
+                TargetLayer.Add(previewPointFeature = new Feature { Geometry = previewPoint });
+            }
+            previewPointFeature.Geometry = previewPoint;
+
+            Update();
+        }
+
+        private Mapsui.Geometries.Point GetGlobalPointFromEvent(MouseEventArgs e)
+        {
+            var screenPosition = e.GetPosition(MapControl).ToMapsui();
+            var globalPosition = MapControl.Viewport.ScreenToWorld(screenPosition);
+            return globalPosition;
+        }
+
+        private void Update()
+        {
+            TargetLayer.Refresh();
+        }
+
+        public T EndDrawing()
+        {
+            if (!IsInDrawingMode)
+            {
+                return null;
+            }
+
+            IsInDrawingMode = false;
+            T result = CurrentDrawnObject;
+
+            MapControl.Cursor = Cursors.Arrow;
+
+            if (CurrentDrawnObject != null)
+            {
+                if (previewPoint != null)
+                {
+                    CurrentDrawnObject.Vertices.Remove(previewPoint);
+                }
+                if (CurrentDrawnObject.Vertices.Count <= 1)
+                {
+                    TargetLayer.TryRemove(CurrentDrawnObject);
+                    result = null;
+                }
+            }
+            if (previewPointFeature != null)
+            {
+                TargetLayer.TryRemove(previewPointFeature);
+            }
+            Update();
+
+            CurrentDrawnObject = null;
+            previewPoint = null;
+            previewPointFeature = null;
+
+            return result;
+        }
+
+        protected abstract T CreateNewMapObject();
+    }
+}
--- TrailEvolutionModelling/EditorTools/PolygonTool.cs
+++ TrailEvolutionModelling/EditorTools/PolygonTool.cs
@@ -15,145 +15,16 @@ using TrailEvolutionModelling.Util;
 
 namespace TrailEvolutionModelling.EditorTools
 {
-    class PolygonTool
+    class PolygonTool : MapObjectTool<Polygon>
     {
-        private MapControl mapControl;
-        private WritableLayer polygonLayer;
-
-        private AreaType currentAreaType;
-        private System.Windows.Point mouseDownPos;
-        private Polygon currentPolygon;
-        private Mapsui.Geometries.Point previewPoint;
-        private Feature previewPointFeature;
-
-        public bool IsInDrawingMode { get; private set; }
-
         public PolygonTool(MapControl mapControl, WritableLayer polygonLayer)
+            : base(mapControl, polygonLayer)
         {
-            this.mapControl = mapControl;
-            this.polygonLayer = polygonLayer;
-
-            this.mapControl.MouseLeftButtonDown += OnLeftMouseDown;
-            this.mapControl.MouseLeftButtonUp += OnLeftMouseUp;
-            this.mapControl.MouseMove += OnMouseMove;
         }
 
-        private void OnLeftMouseDown(object sender, MouseButtonEventArgs e)
+        protected override Polygon CreateNewMapObject()
         {
-            if (!IsInDrawingMode)
-            {
-                return;
-            }
-            mouseDownPos = e.GetPosition(mapControl);
-        }
-
-        private void OnLeftMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (!IsInDrawingMode)
-            {
-                return;
-            }
-            const double tolerance = 5;
-            var pos = e.GetPosition(mapControl);
-            if (Math.Abs(pos.X - mouseDownPos.X) > tolerance ||
-                Math.Abs(pos.Y - mouseDownPos.Y) > tolerance)
-            {
-                return;
-            }
-            // previewPoint is already in currentPolygon.
-            // Setting it null makes OnMouseMove not remove it from currentPolygon
-            // thus it stays persistently
-            previewPoint = null;
-        }
-
-        private void OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (!IsInDrawingMode)
-            {
-                return;
-            }
-            
-            if (currentPolygon == null)
-            {
-                currentPolygon = new Polygon();
-                currentPolygon.AreaType = currentAreaType;
-                polygonLayer.Add(currentPolygon);
-            }
-            if (previewPoint != null)
-            {
-                currentPolygon.Vertices.Remove(previewPoint);
-            }
-            previewPoint = GetGlobalPointFromEvent(e);
-            currentPolygon.Vertices.Add(previewPoint);
-
-            if (previewPointFeature == null)
-            {
-                polygonLayer.Add(previewPointFeature = new Feature { Geometry = previewPoint });
-            }
-            previewPointFeature.Geometry = previewPoint;
-
-            Update();
-        }
-
-        private Mapsui.Geometries.Point GetGlobalPointFromEvent(MouseEventArgs e)
-        {
-            var screenPosition = e.GetPosition(mapControl).ToMapsui();
-            var globalPosition = mapControl.Viewport.ScreenToWorld(screenPosition);
-            return globalPosition;
-        }
-
-        private void Update()
-        {
-            polygonLayer.Refresh();
-        }
-
-        public void BeginDrawing(AreaType areaType)
-        {
-            if (IsInDrawingMode)
-            {
-                return;
-            }
-
-            IsInDrawingMode = true;
-            currentAreaType = areaType;
-            mapControl.Cursor = Cursors.Pen;
-        }
-
-        public Polygon EndDrawing()
-        {
-            if (!IsInDrawingMode)
-            {
-                return null;
-            }
-
-            IsInDrawingMode = false;
-            Polygon result = currentPolygon;
-
-            mapControl.Cursor = Cursors.Arrow;
-
-            if (currentPolygon != null)
-            {
-                if (previewPoint != null)
-                {
-                    currentPolygon.Vertices.Remove(previewPoint);
-                }
-                if (currentPolygon.Vertices.Count <= 1)
-                {
-                    polygonLayer.TryRemove(currentPolygon);
-                    result = null;
-                }
-            }
-            if (previewPointFeature != null)
-            {
-                polygonLayer.TryRemove(previewPointFeature);
-            }
-            Update();
-
-            currentPolygon = null;
-            previewPoint = null;
-            previewPointFeature = null;
-
-            return result;
+            return new Polygon();
         }
     }
 }
