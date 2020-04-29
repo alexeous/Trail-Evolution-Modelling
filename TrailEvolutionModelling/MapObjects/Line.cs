e497dd1151b8f5e7a50083e38018185ba017efa9
Alex <alexey-bycov@yandex.ru>
2020-04-29 23:49:56 +0700

    Add Line the MapObject

---
+++ TrailEvolutionModelling/MapObjects/Line.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mapsui.Geometries;
+
+namespace TrailEvolutionModelling.MapObjects
+{
+    class Line : MapObject
+    {
+        public override IList<Point> Vertices => LineString.Vertices;
+
+        private LineString LineString => (LineString)Geometry;
+
+        protected override IGeometry CreateGeometry()
+        {
+            return new LineString();
+        }
+
+        protected override void InitGeometryFromText(string geometryText)
+        {
+            if (string.IsNullOrWhiteSpace(geometryText))
+            {
+                throw new ArgumentException($"{nameof(geometryText)} is null or blank");
+            }
+
+            LineString lineString;
+            try
+            {
+                var geometry = Mapsui.Geometries.Geometry.GeomFromText(geometryText);
+                lineString = geometry as LineString;
+                if (lineString == null)
+                {
+                    throw new ArgumentException($"Geometry text is invalid: {geometryText}", nameof(geometryText));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"Geometry text is invalid: {geometryText}", nameof(geometryText), ex);
+            }
+
+            Geometry = lineString;
+        }
+    }
+}
