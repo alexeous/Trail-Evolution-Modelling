a6fdcc1ec2e7bba3d05aa70682b211ebcb36aa25
Alex <alexey-bycov@yandex.ru>
2020-05-15 01:12:42 +0700

    Introduce NodesDataDevicePair<T> template class and make ComputeNodesPair an instance of it

--- TrailsGPUProxy/ComputeNodesPair.h
+++ TrailsGPUProxy/ComputeNodesPair.h
@@ -1,25 +1,11 @@
 #pragma once
-#include "IResource.h"
-#include "ResourceManager.h"
+#include "NodesDataDevicePair.h"
 #include "ComputeNode.h"
-#include "NodesDataHaloed.h"
 
 namespace TrailEvolutionModelling {
 	namespace GPUProxy {
 
-		struct ComputeNodesPair : public IResource {
-			friend class ResourceManager;
-
-			NodesDataHaloedDevice<ComputeNode>* readOnly = nullptr;
-			NodesDataHaloedDevice<ComputeNode>* writeOnly = nullptr;
-
-			void CopyReadToWrite(int graphW, int graphH, cudaStream_t stream = 0);
-			void CopyWriteToRead(int graphW, int graphH, cudaStream_t stream = 0);
-
-		protected:
-			ComputeNodesPair(int graphW, int graphH, ResourceManager* resources);
-			void Free(ResourceManager& resources) override;
-		};
+		using ComputeNodesPair = NodesDataDevicePair<ComputeNode>;
 
 	}
 }
